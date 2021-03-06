c51c631423d3ffbf2a4d8637613d5a142ab27062
enuelgom <e.gm@live.com.mx>
2021-03-05 21:49:28 -0600

    7

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,7 @@ const {ApolloServer} = require('apollo-server-express')
 import express from 'express';
 import { resolvers, typeDefs } from "../graphql";
 import http, { createServer } from "http";
+const expressPlayground = require("graphql-playground-middleware-express").default;
 
 import cors from "cors"
 import bodyParser from "body-parser"
@@ -53,14 +54,14 @@ server.installSubscriptionHandlers(httpserver)
 // httpserver.listen({ port: process.env.PORT}, (url)=> {
 //     console.log(`🚀 Server ready at ${url}:${server.graphqlPath}`);
 //   });
-httpserver.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
+// httpserver.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+//     console.log(`🚀 Server ready at ${url}`);
+//   });
 
-// app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 
-// const port = process.env.PORT || "4000";
+const port = process.env.PORT || "4000";
 
-// app.listen(port);
+app.listen(port);
 
-// console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
